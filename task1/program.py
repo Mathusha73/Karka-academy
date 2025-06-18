b140b1845f29e974eed43c8101a6afcefa703577
Mathusha73 <mahibav7@gmail.com>
2025-06-18 17:47:31 +0530

    Add files via upload

---
+++ task1/program.py
@@ -0,0 +1,77 @@
+# #1
+# print("Hello,World!")
+
+# #2
+# print("Mathusha")
+
+# #3
+# print("Name:Mathusha\nAge:21 years")
+
+# #4
+# name="Mathusha" #string
+# age=21 #integer
+# percentage=84.5 #float
+# is_pass=True #boolean
+# print("I am",name,"My age is",age,"years.")
+# print("Mathusha has passed with",percentage,"percentage")
+
+# #5
+# name="Mathusha\t"
+# age="21 years"
+# print(name +age)
+
+#6
+name="V.S.Mathusha"
+age="21 years"
+print(name)
+print(age)
+
+#7
+a=10
+b=24
+addition=a+b
+subraction=b-a
+multiplication=a*b
+division=a/b
+print("addition=",addition)
+print("Subraction=",subraction)
+print("Multiplication=",multiplication)
+print("Division=",division)
+
+#8
+print("*")
+print("* *")
+print("* * *")
+print("* * * *")
+print("* * * * *")
+
+#9
+name="Veena"
+age=20
+isStudent=True
+print(name)
+print(age)
+print(isStudent)
+name="Priya"
+age=age+1
+status=not isStudent
+print(name)
+print(age)
+print(status)
+
+#10
+
+name="Varshini"
+age=20
+dob="18-9-2004"
+height=5.4
+weight=53.1
+degree="B.E"
+gender="Female" 
+print(name)
+print(age)
+print(dob)
+print(height)
+print(weight)
+print(degree)
+print(gender)
